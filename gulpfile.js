9564899ecee0078dc100f2265ed7e031cb2f5f19
Diwesh Saxena <diweshsaxena@gmail.com>
2016-08-25 14:36:55 +0900

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
+	browserify = require('gulp-browserify'),
 	coffee = require('gulp-coffee'),
 	concat = require('gulp-concat');
+	
 
 var coffeeSources = ['components/coffee/tagline.coffee']
 var jsSources = [
@@ -22,5 +24,6 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('scripts.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
